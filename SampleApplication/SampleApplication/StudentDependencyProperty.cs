1e7ec35f6ed6ee06b607c1e461ee529c121991e5
shaifulullah <shaiful.ullah@yahoo.com>
2018-01-23 20:36:58 -0500

    Part 13

---
+++ SampleApplication/SampleApplication/StudentDependencyProperty.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SampleApplication
+{
+    public class StudentDependencyProperty : DependencyObject
+    {
+        public static readonly DependencyProperty StudentProperty = DependencyProperty
+            .Register("NameProperty", typeof(string), typeof(MainWindow));
+
+        public string StudentName
+        {
+            get
+            {
+                return (string)GetValue(StudentProperty);
+            }
+            set
+            {
+                SetValue(StudentProperty, value);
+            }
+
+        }
+    }
+}
